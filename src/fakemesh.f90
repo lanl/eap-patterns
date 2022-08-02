679d056ebd11dbf9a7474ae4954ee1283d356748
Sriram Swaminarayan <sriram@lanl.gov>
2022-08-02 16:06:08 -0600

    Fixed parallel bug in material variable

--- src/fakemesh.f90
+++ src/fakemesh.f90
@@ -128,7 +128,7 @@ contains
          !loCell =>  pio_get_range_i64(self%id, "cell_index", 2 * idim - 1, iStart, nCount)
 
          ! Get the material counts
-         frac_core%vol%obj = pio_get_range_matvar(self%ID, "chunk_vol", iStart, nCount) 
+         frac_core%vol%obj = pio_get_range_matvar(self%ID, "chunk_vol", 0, nCount) 
 
          
     END ASSOCIATE
